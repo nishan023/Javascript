8a53f017d902dbcb2e03818772600141e8ec16ca
Nissan Dhakal <nissandhakal11@gmail.com>
2023-12-26 18:37:10 +0545

    First commit

---
+++ Java_Script/Everyday_Karma/bulitin.js
@@ -0,0 +1,5 @@
+console.log(Math.pow(2,5));
+console.log(Math.ceil(5.0));
+console.log(Math.random());
+console.log(new Date().getFullYear());
+console.log(new Date().toISOString().split("T")[0]);
--- Java_Script/bulitin.js
+++
@@ -1,2 +0,0 @@
-console.log(Math.pow(2,4));
-console.log(Math.ceil(5.13));
